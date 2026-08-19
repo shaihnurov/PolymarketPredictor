05a7cf2fe6f3ea246b78dcdc1826f5b3501a665a
Ilnar Shaihnurov <me@shaihnurov.ru>
2026-08-19 14:42:10 +0500

    chore: Clean Architecture слоями
    - 4 слоя (Domain/Application/Infrastructure/WebApi) + 2 тестовых проекта
    - граф ссылок: Application -> Domain, Infrastructure -> Application, WebApi -> Application+Infrastructure

---
+++ tests/PolymarketPredictor.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace PolymarketPredictor.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/PolymarketPredictor.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace PolymarketPredictor.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
